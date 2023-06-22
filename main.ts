5a0fdf5c7169b9c06ace32d162431b28f5ab8d28
Yuichiroh-Kobayashi <qcho0613@gmail.com>
2023-06-22 22:34:31 +0900

    A+Bボタンでデータリセットする機能を追加

--- main.ts
+++ main.ts
@@ -1,25 +1,33 @@
+datalogger.onLogFull(function () {
+    basic.showIcon(IconNames.Yes)
+    Loop = 0
+    LoopCount = 0
+})
 // Aボタンで測定開始
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
     Loop = 1
 })
-// Bボタンで測定終了
-input.onButtonPressed(Button.B, function () {
+// A+Bボタンでデータクリア
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.No)
+    datalogger.deleteLog(datalogger.DeleteType.Full)
     Loop = 0
     LoopCount = 0
-    basic.showIcon(IconNames.Happy)
+    control.reset()
 })
 let LoopCount = 0
 let Loop = 0
 Loop = 0
 LoopCount = 0
-let DataPlot = 500
+let DataPlot = 300
 datalogger.includeTimestamp(FlashLogTimeStampFormat.Seconds)
 datalogger.setColumnTitles("P0")
 basic.showIcon(IconNames.Happy)
-// 最短20ms
+// 測定周期：最短20ms
+// 測定点数：変数DataPlotで設定
 loops.everyInterval(20, function () {
-    if (Loop == 1 && LoopCount < DataPlot) {
+    if (Loop == 1 && LoopCount <= DataPlot) {
         datalogger.log(datalogger.createCV("P0", pins.analogReadPin(AnalogPin.P0)))
         LoopCount += 1
     } else if (Loop == 1 && LoopCount > DataPlot) {
